85b9dcc8573ebc4dc1523eeaf0862aac53e2de02
mostafa69d <mostafa69d@gmail.com>
2017-09-15 11:19:01 -0400

    feat: create book component

---
+++ src/components/Book.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+class Book extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    bookId=this.props.id;
+    render(){
+        return(
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("http://books.google.com/books/content?id=${{bookId}}")` }}></div>
+                    <div className="book-shelf-changer">
+                        <select onChange={(e) => this.props.updateShelf(e.target.value)}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{this.props.title}</div>
+                <div className="book-authors">{this.props.author}</div>
+            </div>
+        )
+    }
+
+}
+
+export default Book;
