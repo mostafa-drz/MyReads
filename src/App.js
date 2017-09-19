47b2b5f88aaf6b0b9661d0c10ecbf79b5f75d476
mostafa69d <mostafa69d@gmail.com>
2017-09-19 16:57:25 -0400

    feat: add router to app

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ import './App.css'
 import BookShelf      from    './components/BookShelf'
 import SearchResults  from    './components/SearchResults'
 import SearchBar      from    './components/SearchBar'
-
+import {Route,Link}        from    'react-router-dom'
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books:[],
     shelves:[],
     searchText:''
@@ -30,60 +23,60 @@ class BooksApp extends React.Component {
     })
   }
   render() {
-    return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <SearchBar onSearchChange={(text) => {
-              this.setState((prevState) =>({
-                searchText:text
-              }))
-            }}/>
-            <SearchResults
-             search={this.state.searchText}
-             currentBooks={this.state.books}
-             updateShelf={(book,shelf) => {
-                BooksAPI.update(book,shelf);
-                let newState=this.state.books.filter((b) => (book.id)!==b.id)
-                book.shelf=shelf;
-                newState.push(book);
-                this.setState((prevState) => ({
-                  books:newState
-                }))
-            }}
-            />
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                {this.state.shelves.map((shelf) => ( shelf.type!=='none' &&
-                  <BookShelf 
-                  title={shelf.title}
-                  books={this.state.books.filter((book) => (book.shelf)===shelf.type)}
-                  key={shelf.type}
-                  updateShelf={(book,shelf) => {
-                    BooksAPI.update(book,shelf);
-                    let newState=this.state.books.filter((b) => (book.id)!==b.id)
-                    book.shelf=shelf;
-                    newState.push(book);
-                    this.setState((prevState) => ({
-                      books:newState
+      return (
+        <div className="app">
+            <Route exact path='/search' render={() => (
+              <div className="search-books">
+                  <SearchBar onSearchChange={(text) => {
+                    this.setState((prevState) =>({
+                      searchText:text
                     }))
-                  }}
-                  />
-                ))}
+                  }}/>
+                <SearchResults
+                  search={this.state.searchText}
+                  currentBooks={this.state.books}
+                  updateShelf={(book,shelf) => {
+                      BooksAPI.update(book,shelf);
+                      let newState=this.state.books.filter((b) => (book.id)!==b.id)
+                      book.shelf=shelf;
+                      newState.push(book);
+                      this.setState((prevState) => ({
+                        books:newState
+                      }))
+                  }}/>
               </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
-      </div>
+            )}/>
+            <Route exact path="/" render={() =>(
+              <div className="list-books">
+                  <div className="list-books-title">
+                    <h1>MyReads</h1>
+                  </div>
+                  <div className="list-books-content">
+                    <div>
+                      {this.state.shelves.map((shelf) => ( shelf.type!=='none' &&
+                        <BookShelf 
+                        title={shelf.title}
+                        books={this.state.books.filter((book) => (book.shelf)===shelf.type)}
+                        key={shelf.type}
+                        updateShelf={(book,shelf) => {
+                          BooksAPI.update(book,shelf);
+                          let newState=this.state.books.filter((b) => (book.id)!==b.id)
+                          book.shelf=shelf;
+                          newState.push(book);
+                          this.setState((prevState) => ({
+                            books:newState
+                          }))
+                        }}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                  <div className="open-search">
+                    <Link to='/search'>Add a book</Link>
+                  </div>
+              </div>
+            )}/>
+        </div>
     )
   }
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 class SearchBar extends React.Component{
     render(){
         return(
             <div className="search-books-bar">
-                <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                <Link className="close-search" to="/">Close</Link>
                 <div className="search-books-input-wrapper">
                     <input type="text" placeholder="Search by title or author"
                     onChange={(e) => this.props.onSearchChange(e.target.value)}
